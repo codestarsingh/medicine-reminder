30c340b386187712a10222f3229b28abef07c391
Shubham <singh66333@gmail.com>
2023-08-19 08:23:17 +0530

    ⚡️ Add App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from './Dashboard/Dashboard.jsx'
+import Login from './components/login.component'
+import SignUp from './components/signup.component'
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div>                
+                <Routes>                       
+                    <Route exact path="/" element={<Login />} />
+                    <Route path="/sign-up" element={<SignUp />} />
+                    <Route path="/dashboard" exact element={<Dashboard/>}/>
+                </Routes>                    
+            </div>
+        </BrowserRouter>
+    );
+}
+
+export default App;
