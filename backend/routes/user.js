9ae84169bc84cc3bba027ec119f4ea2dd2e42681
Shubham <singh66333@gmail.com>
2023-08-23 12:25:53 +0530

    🚚 Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,9 +4,9 @@ const userController = require("../controllers/user");
 
 router.post("/add-user", userController.addUser);
 router.post("/delete-user", userController.deleteUser);
-router.post("/edit-phono",userController.editPhoneNumber);
-router.post("/edit-user",userController.editUser);
-router.get("/getAllUsers",userController.getAllUsers);
-router.get("/searchByUserName",userController.searchByUserName);
+router.post("/edit-user", userController.editUser);
+router.post("/edit-phono", userController.editPhoneNumber);
+router.get("/getAllUsers", userController.getAllUsers);
+router.get("/searchByUserName", userController.searchByUserName);
 
 module.exports = router;
