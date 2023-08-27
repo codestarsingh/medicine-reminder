2091fdaedd342f5c32ab0ae9c18509f33f65c48d
Shubham <singh66333@gmail.com>
2023-08-27 08:37:39 +0530

    ⚡️ Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,22 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cron = require('./cron');
+const route = require('./routes/route');
 require('dotenv').config();
-require('./models/userDetails');
-const userRoute = require('./routes/user');
+require('./models/adminInfo');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
-app.use(userRoute);
-const User = mongoose.model('userInfo');
+app.use(route);
 
+const adminInfo = mongoose.model('adminInfo');
 const MONGOURL = process.env.MONGOURL;
 const PORT = process.env.PORT;
 const JWT_SECRET = process.env.JWT_SECRET;
@@ -36,17 +36,33 @@ mongoose.connection.on('disconnected', () => {
   console.log('Mongoose is still disconnected');
 });
 
-app.post('/register', async (req, res) => {
-  const { fname, lname, email, password } = req.body;
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  const admin = await adminInfo.findOne({ email });
+  if (!admin) {
+    return res.json({ message: 'Admin not found' });
+  }
+  if (await bcrypt.compare(password, admin.password)) {
+    const token = jwt.sign({ email: admin.email }, JWT_SECRET);
+    if (res.status(200)) {
+      return res.json({ status: 'ok', token: token });
+    } else {
+      return res.json({ message: 'error' });
+    }
+  }
+  res.json({ status: 'error', message: 'Invalid password' });
+});
+
+app.post('/signup', async (req, res) => {
+  const { name, email, password } = req.body;
   const encryptedPassword = await bcrypt.hash(password, 10);
   try {
-    const oldUser = await User.findOne({ email });
-    if (oldUser) {
-      return res.json({ error: 'User Exists' });
+    const oldAdmin = await adminInfo.findOne({ email });
+    if (oldAdmin) {
+      return res.json({ error: 'Admin exists' });
     }
-    await User.create({
-      fname,
-      lname,
+    await adminInfo.create({
+      name,
       email,
       password: encryptedPassword
     });
@@ -56,40 +72,8 @@ app.post('/register', async (req, res) => {
   }
 });
 
-app.post('/login-user', async (req, res) => {
-  const { email, password } = req.body; // at first we have collected email & password here 
-  const user = await User.findOne({ email }); // we checked whether that email exists or not
-  if (!user) {
-    return res.json({ error: 'User not found' });
-  }
-  if (await bcrypt.compare(password, user.password)) { // we compared the password with user password
-    const token = jwt.sign({ email: user.email }, JWT_SECRET); // if the password is true then it will generate jwt token with the use of our jwt secret
-    if (res.status(201)) { // if everything goes well it would return a token with status ok
-      return res.json({ status: 'ok', data: token });
-    } else {
-      return res.json({ error: 'error' });
-    }
-  }
-  res.json({ status: 'error', error: 'Invalid Password' });
-});
-
-app.post('/userData', async (req, res) => {
-  const { token } = req.body;
-  try {
-    const user = jwt.verify(token, JWT_SECRET);
-    const useremail = user.email;
-    User.findOne({ email: useremail })
-      .then((data) => {
-        res.send({ status: 'ok', data: data });
-      })
-      .catch((error) => {
-        res.send({ status: 'error', data: error });
-      });
-  } catch (error) { }
-});
-
-cron.sendMailAllUser();
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+cron.sendMailAllUser(); //this was a good 
