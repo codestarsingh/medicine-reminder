d454cde6fcf72172a2f8ed60f01d2342ef6e83e6
Shubham <singh66333@gmail.com>
2023-08-27 08:49:22 +0530

    📦 Update DeleteModal.jsx

--- frontend/src/Modals/DeleteModal/DeleteModal.jsx
+++ frontend/src/Modals/DeleteModal/DeleteModal.jsx
@@ -1,39 +1,36 @@
-import React, { useState } from "react";
-import axios from "axios";
-import "./DeleteModal.css"
+import React, { useState } from 'react';
+import axios from 'axios';
+import './DeleteModal.css';
 
 const DeleteModal = ({ closeModal, user }) => {
-    const [error, setError] = useState("");
+    const [error, setError] = useState('');
     const handleDelete = () => {
-        axios.post("http://localhost:5000/delete-user", { phoneNumber: user.phoneNumber })
+        axios.post('http://localhost:5000/delete-user', { phoneNumber: user.phoneNumber })
             .then(response => {
-                if (response.data.message == "User Deleted") {
-                    setError("");
+                if (response.data.message == 'User Deleted') {
+                    setError('');
                     closeModal();
                 } else {
                     setError(response.data.message);
                 }
-            })
-            .catch(err => {
+            }).catch(err => {
                 setError(err);
-            })
+            });
     }
     
     return (
-        <div className="modal-container">
-            <div className="modal-header">
+        <div>
+            <div>
                 <h2>Delete Patient</h2>
-                <button onClick={closeModal} className="modal-close-btn">
-                    &times;
-                </button>
+                <button onClick={closeModal}>&times;</button>
             </div>
-            <div className="modal-body">
-                <p className="deleteHeader">Are You Sure You Want To Delete User ?</p>
-                <div className="deleteButtons">
-                    <button className="danger" onClick={() => handleDelete()}>YES</button>
-                    <button className="safe" onClick={closeModal}>NO</button>
+            <div>
+                <p className='deleteHeader'>Are you sure you want to delete user ?</p>
+                <div className='deleteButtons'>
+                    <button onClick={() => handleDelete()}>YES</button>
+                    <button className='safe' onClick={closeModal}>NO</button>
                 </div>
-                <div className="errorTag">{error}</div>
+                <div>{error}</div>
             </div>
         </div>
     );
