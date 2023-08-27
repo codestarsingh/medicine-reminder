a43e6561cc636aa72f6e1d540fa9a13a9fa93405
Shubham <singh66333@gmail.com>
2023-08-27 12:39:02 +0530

    📦 Update DeleteModal.jsx

--- frontend/src/Modals/DeleteModal/DeleteModal.jsx
+++ frontend/src/Modals/DeleteModal/DeleteModal.jsx
@@ -7,7 +7,7 @@ const DeleteModal = ({ closeModal, user }) => {
     const handleDelete = () => {
         axios.post('http://localhost:5000/delete-user', { phoneNumber: user.phoneNumber })
             .then(response => {
-                if (response.data.message == 'User Deleted') {
+                if (response.data.message == 'User deleted') {
                     setError('');
                     closeModal();
                 } else {
@@ -19,18 +19,20 @@ const DeleteModal = ({ closeModal, user }) => {
     }
     
     return (
-        <div>
-            <div>
+        <div className='modal-container'>
+            <div className='modal-header'>
                 <h2>Delete Patient</h2>
-                <button onClick={closeModal}>&times;</button>
+                <button onClick={closeModal} className='modal-close-btn'>
+                    &times;
+                </button>
             </div>
-            <div>
+            <div className='modal-body'>
                 <p className='deleteHeader'>Are you sure you want to delete user ?</p>
                 <div className='deleteButtons'>
-                    <button onClick={() => handleDelete()}>YES</button>
+                    <button className='danger' onClick={() => handleDelete()}>YES</button>
                     <button className='safe' onClick={closeModal}>NO</button>
                 </div>
-                <div>{error}</div>
+                <div className='errorTag'>{error}</div>
             </div>
         </div>
     );
