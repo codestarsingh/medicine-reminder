78f26919c07a3162356ee5b3855bfb4b51692269
Shubham <singh66333@gmail.com>
2023-08-27 12:39:23 +0530

    📦 Update PhoneModal.jsx

--- frontend/src/Modals/PhoneModal/PhoneModal.jsx
+++ frontend/src/Modals/PhoneModal/PhoneModal.jsx
@@ -1,47 +1,44 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const PhoneModal = ({ closeModal, user }) => {
     const oldNum = user.phoneNumber;
-    const [error, setError] = useState("");
+    const [error, setError] = useState('');
     const [phoneNumberNow, setPhoneNumber] = useState(user.phoneNumber);
     const handleChangePhoneNumber = () => {
-        axios.post("http://localhost:5000/edit-phoneno", { newPhoneNum: phoneNumberNow, oldNum: oldNum })
+        axios.post('http://localhost:5000/edit-phoneno', { newPhoneNum: phoneNumberNow, oldNum: oldNum })
             .then(response => {
-                if (response.data.message == "Phone Number Updated") {
-                    setError("");
+                if (response.data.message == 'Phone number updated') {
+                    setError('');
                     closeModal();
                 } else {
                     setError(response.data.message);
                 }
-            })
-            .catch(err => {
+            }).catch(err => {
                 setError(err);
-            })
+            });
     }
     
     return (
-        <div className="modal-container">
-            <div className="modal-header">
+        <div className='modal-container'>
+            <div className='modal-header'>
                 <h2>Change Phone Number</h2>
-                <button onClick={closeModal} className="modal-close-btn">
-                    &times;
-                </button>
+                <button onClick={closeModal} className='modal-close-btn'>&times;</button>
             </div>
-            <div className="modal-body">
-                <div className="form-group">
-                    <label htmlFor="phoneNumber">Phone Number:</label>
+            <div className='modal-body'>
+                <div className='form-group'>
+                    <label htmlFor='phoneNumber'>Phone Number:</label>
                     <input
-                        type="text"
-                        id="phoneNumber"
+                        type='text'
+                        id='phoneNumber'
                         value={phoneNumberNow}
                         onChange={(e) => setPhoneNumber(e.target.value)}
                     />
                 </div>
-                <div className="deleteButtonsPhone">
-                    <button className="dangerPhone" onClick={() => handleChangePhoneNumber()}>Change Phone Number</button>
+                <div className='deleteButtonsPhone'>
+                    <button style={{marginTop: 25, marginLeft: 538}} className='dangerPhone' onClick={() => handleChangePhoneNumber()}>Change Phone Number</button>
                 </div>
-                <div className="errorTag">{error}</div>
+                <div className='errorTag'>{error}</div>
             </div>
         </div>
     );
