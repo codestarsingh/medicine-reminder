b0318ed42f00ba61666a64c29b3c8fb0307803e8
Shubham <singh66333@gmail.com>
2023-09-04 09:08:22 +0530

    🚚 Update route.js

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const middleware = require('../middleware/auth');
 
 router.post('/add-user', userController.addUser);
 router.post('/delete-user', userController.deleteUser);
@@ -8,5 +9,9 @@ router.post('/edit-user', userController.editUser);
 router.post('/edit-phoneno', userController.editPhoneNumber);
 router.get('/getAllUsers', userController.getAllUsers);
 router.get('/searchByUserName', userController.searchByUserName);
+
+router.get('/test', middleware, function(req, res) {
+    res.status(200).send({success:true, msg: 'authenticated'});
+});
 
 module.exports = router;
