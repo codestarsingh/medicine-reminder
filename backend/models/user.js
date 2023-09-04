f2e69c082965ceed318b1fae178623362db5a724
Shubham <singh66333@gmail.com>
2023-09-04 09:47:53 +0530

    🗃️ Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
     {
-        name: { type: String},
-        phoneNumber: { type: String, unique: true },
-        email: { type: String, unique: true },
-        medicines: [{ medicineName: String, dosage: String, days: String }],
+        name: {type: String},
+        phoneNumber: {type: String, unique: true},
+        email: {type: String, unique: true},
+        medicines: [{medicineName: String, dosage: String, days: String}],
         opted: Boolean
     }, 
     {
